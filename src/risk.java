7a7af44df1a49bfaa1acf2bf467cf0921d47656a
Tibor Billes <billes@gmx.com>
2011-10-20 19:55:55 +0200

    refactor: give the code some directory structure, and put it under one main()
    A new class Risk was added as main(). Client and server network functions
    were put in separate classes, under directory network.

---
+++ src/risk.java
@@ -0,0 +1,15 @@
+
+class Risk {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            if (args[0].compareTo("server") == 0) {
+                server s = new server();
+                s.doit();
+            } else if (args[0].compareTo("client") == 0) {
+                client c = new client();
+                c.doit();
+            }
+        }
+    }
+	
+};
