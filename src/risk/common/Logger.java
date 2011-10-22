1ab152f88f47502d95db43ec55d0787a8431a3b3
Tibor Billes <tbilles@gmx.com>
2011-10-22 15:33:33 +0200

    logger: modified logger to handle nested exceptions

--- src/risk/common/Logger.java
+++ src/risk/common/Logger.java
@@ -14,11 +14,11 @@ public class Logger {
     private LogLevel logLevel = LogLevel.TRACE;
     static private Logger instance = new Logger();
     private final String endLine = "\r\n";
-    
+
     static public Logger getInstance() {
         return instance;
     }
-    
+
     public void initialize(boolean logToConsole, String logFile) {
         this.logToConsole = logToConsole;
         this.logFile = logFile;
@@ -31,25 +31,25 @@ public class Logger {
         }
         initialized = true;
     }
-    
+
     private void log(LogLevel level, String msg) {
         if (!initialized)
             throw new RuntimeException("Logger not initialized!");
-        
+
         if (level.ordinal() < logLevel.ordinal()) {
-            return ;
+            return;
         }
-        
+
         StringBuilder logMsg = new StringBuilder();
         Date d = new Date();
-        
+
         logMsg.append(d.toString());
         logMsg.append(" - [");
         logMsg.append(level.toString());
         logMsg.append("] ");
         logMsg.append(msg);
         logMsg.append(endLine);
-        
+
         if (logFile != null) {
             try {
                 wr.write(logMsg.toString());
@@ -58,18 +58,23 @@ public class Logger {
                 e.printStackTrace();
             }
         }
-        
+
         if (logToConsole) {
             System.out.print(logMsg.toString());
         }
     }
-    
+
     private void log(Exception e, String msg) {
         StringBuilder sb = new StringBuilder();
         sb.append(msg + endLine);
+        appendException(e, sb);
+
+        log(LogLevel.ERROR, sb.toString());
+    }
+
+    private void appendException(Throwable e, StringBuilder sb) {
         sb.append("    Exception: ");
         sb.append(e.toString());
-        sb.append(e.getMessage());
         sb.append(endLine);
         sb.append("    Stack trace:" + endLine);
         StackTraceElement[] stkElements = e.getStackTrace();
@@ -78,30 +83,31 @@ public class Logger {
             sb.append("at " + stkFrame.getFileName() + ":" + stkFrame.getLineNumber());
             sb.append(endLine);
         }
-        
-        log(LogLevel.ERROR, sb.toString());
+        if (e.getCause() != null) {
+            appendException(e.getCause(), sb);
+        }
     }
-    
+
     static public void logtrace(String msg) {
         getInstance().log(LogLevel.TRACE, msg);
     }
-    
+
     static public void logdebug(String msg) {
         getInstance().log(LogLevel.DEBUG, msg);
     }
-    
+
     static public void loginfo(String msg) {
         getInstance().log(LogLevel.INFO, msg);
     }
-    
+
     static public void logwarn(String msg) {
         getInstance().log(LogLevel.WARNING, msg);
     }
-    
+
     static public void logerror(String msg) {
         getInstance().log(LogLevel.ERROR, msg);
     }
-    
+
     static public void logexception(Exception e, String msg) {
         getInstance().log(e, msg);
     }
