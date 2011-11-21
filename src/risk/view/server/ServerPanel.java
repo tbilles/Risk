eee4d1561d235ec37203218f6fccfcac7b587fdf
sebokt <sebokt89@gmail.com>
2011-11-21 23:05:45 +0100

    server messages on scrollpane

--- src/risk/view/server/ServerPanel.java
+++ src/risk/view/server/ServerPanel.java
@@ -5,6 +5,7 @@ import java.awt.BorderLayout;
 import risk.common.RiskIO;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class ServerPanel extends JPanel {
@@ -12,7 +13,8 @@ public class ServerPanel extends JPanel {
     public ServerPanel() {
         JTextArea temp = RiskIO.getServerTextArea();
         setLayout(new BorderLayout());
-        add(temp, BorderLayout.CENTER);
-
+        JScrollPane sp=new JScrollPane();
+        sp.setViewportView(temp);
+        add(sp, BorderLayout.CENTER);
     }
 }
