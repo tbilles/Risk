accc1ad0b3a91e67daeddd70606b8faa499a3191
sebokt <sebokt89@gmail.com>
2011-11-21 22:44:14 +0100

    null pointer exception @regrouping solved

--- src/risk/protocol/ServerCommandVisitor.java
+++ src/risk/protocol/ServerCommandVisitor.java
@@ -19,12 +19,13 @@ public class ServerCommandVisitor implements CommandVisitor {
     private GameController gameCtrl;
     private ArrayList<Color> colors;
 
-    public ServerCommandVisitor(CommandSender cmdSender, GameView gameView, GameController gameCtrl, ClientHandler clientHandler) {
+    public ServerCommandVisitor(CommandSender cmdSender, GameView gameView,
+            GameController gameCtrl, ClientHandler clientHandler) {
         this.gameView = gameView;
         this.gameCtrl = gameCtrl;
         this.cmdSender = cmdSender;
         this.clientHandler = clientHandler;
-        
+
         colors = new ArrayList<Color>();
         colors.add(new Color(200, 0, 0)); // Red
         colors.add(new Color(0, 0, 200)); // Blue
@@ -44,10 +45,11 @@ public class ServerCommandVisitor implements CommandVisitor {
             sendError(ErrorCmd.NAME_ALREADY_USED);
             return;
         }
-        
-        Player newPlayer = new Player(cmd.getName(), colors.get(gameView.getPlayers().size()));
+
+        Player newPlayer = new Player(cmd.getName(), colors.get(gameView
+                .getPlayers().size()));
         clientHandler.setPlayer(newPlayer);
-        
+
         // Get current players and send them to the new player
         Iterable<Player> players = gameView.getPlayers();
         for (Player p : players) {
@@ -55,10 +57,10 @@ public class ServerCommandVisitor implements CommandVisitor {
         }
 
         // Create new player and send it to everyone
-        
+
         gameCtrl.addPlayer(newPlayer);
         cmdSender.sendPlayerJoinedCmd(newPlayer);
-        
+
         // Check if all the players are here
         if (gameView.getPlayers().size() == 2) {
             startGame();
@@ -101,11 +103,12 @@ public class ServerCommandVisitor implements CommandVisitor {
     private void startGame() {
         // Send game started notification
         cmdSender.sendCmd(new GameStartedCmd(), null);
-        
+
         // Randomly give countries to players
-        ArrayList<Country> countries = new ArrayList<Country>(gameView.getCountries());
+        ArrayList<Country> countries = new ArrayList<Country>(
+                gameView.getCountries());
         Collection<Player> players = gameView.getPlayers();
-        
+
         Random r = new Random();
         Iterator<Player> playerIterator = players.iterator();
         while (!countries.isEmpty()) {
@@ -115,7 +118,8 @@ public class ServerCommandVisitor implements CommandVisitor {
             // Set random country to be owned by the next player
             Country c = countries.get(rand);
             c.setOwner(playerIterator.next());
-            Logger.logdebug("Country name: " + c.getName() + " to " + c.getOwner().getName());
+            Logger.logdebug("Country name: " + c.getName() + " to "
+                    + c.getOwner().getName());
             c.setTroops(1);
             cmdSender.sendCmd(new CountyInitCmd(c), null);
             // Remove Country from the 'unowned' countries list
@@ -125,10 +129,10 @@ public class ServerCommandVisitor implements CommandVisitor {
                 playerIterator = players.iterator();
             }
         }
-        
+
         initNextRound();
     }
-    
+
     private void initNextRound() {
         Logger.logdebug("Initing next round");
         gameCtrl.setRoundPlayers(gameView.getPlayers());
@@ -148,10 +152,12 @@ public class ServerCommandVisitor implements CommandVisitor {
             break;
         }
         gameCtrl.setRoundPhases(phases);
-        cmdSender.sendCmd(new NextRoundCmd(gameView.getPlayers(), gameView.getRoundPhases()), null);
+        cmdSender.sendCmd(
+                new NextRoundCmd(gameView.getPlayers(), gameView
+                        .getRoundPhases()), null);
         gotoNextPlayer();
     }
-    
+
     private void initNextPhase() {
         Logger.logdebug("Initing next phase");
         if (!gameCtrl.swicthToNextPhase()) {
@@ -178,7 +184,7 @@ public class ServerCommandVisitor implements CommandVisitor {
             break;
         }
     }
-    
+
     private void gotoNextPlayer() {
         Logger.logdebug("Initing next player");
         if (!gameCtrl.switchToNextPlayer()) {
@@ -190,14 +196,14 @@ public class ServerCommandVisitor implements CommandVisitor {
         cmdSender.sendCmd(new NextPlayerCmd(), null);
         initNextPhase();
     }
-    
+
     private int getReinforcement(Player p) {
         if (gameView.getRoundNumber() == 1) {
             int playerno = gameView.getPlayers().size() - 2;
             if (playerno > 4) {
                 playerno = 4;
             }
-            return 40 - playerno*5;
+            return 40 - playerno * 5;
         } else {
             Collection<Country> countries = gameView.getCountries();
             int countryno = 0;
@@ -208,7 +214,7 @@ public class ServerCommandVisitor implements CommandVisitor {
                 }
             }
             int reinforcement = countryno / 3;
-            
+
             // Count continents
             Collection<Continent> continents = gameView.getContinents();
             Player myPlayer = gameView.getMyPlayer();
@@ -245,36 +251,49 @@ public class ServerCommandVisitor implements CommandVisitor {
             return 5;
         } else if (continent.getName() == Continent.SOUTHAMERICA) {
             return 2;
-        } 
+        }
         return 0;
     }
 
     @Override
     public void visit(PlaceReinforcementCmd cmd) {
         Country country = gameView.getCountry(cmd.getCountry().getName());
-        Logger.logdebug("Got PlaceReinforcement command to " + country.getName() + " (" + cmd.getTroops() + ")");
+        Logger.logdebug("Got PlaceReinforcement command to "
+                + country.getName() + " (" + cmd.getTroops() + ")");
         if (clientHandler.getPlayer() != gameView.getCurrentPlayer()) {
-            Logger.logwarn(clientHandler.getPlayer().getName() + " is trying to place reinforcement in someone else's turn " + gameView.getCurrentPlayer().getName());
+            Logger.logwarn(clientHandler.getPlayer().getName()
+                    + " is trying to place reinforcement in someone else's turn "
+                    + gameView.getCurrentPlayer().getName());
             sendError(ErrorCmd.ILLEGAL_ARGUMENT);
             return;
         }
-        
-        if (cmd.getTroops() < 0 || cmd.getTroops() > gameView.getAvailableReinforcement()) {
-            Logger.logwarn(clientHandler.getPlayer().getName() + " is trying to place " + cmd.getTroops() + " reinforcement, available is " + gameView.getAvailableReinforcement());
+
+        if (cmd.getTroops() < 0
+                || cmd.getTroops() > gameView.getAvailableReinforcement()) {
+            Logger.logwarn(clientHandler.getPlayer().getName()
+                    + " is trying to place " + cmd.getTroops()
+                    + " reinforcement, available is "
+                    + gameView.getAvailableReinforcement());
             sendError(ErrorCmd.ILLEGAL_ARGUMENT);
             return;
         }
-        
-        if (gameView.getCountry(cmd.getCountry().getName()).getOwner() != clientHandler.getPlayer()) {
-            Logger.logwarn(clientHandler.getPlayer().getName() + " is trying to place reinforcement to " + country.getName() + " (" + country.getOwner().getName() + ")");
+
+        if (gameView.getCountry(cmd.getCountry().getName()).getOwner() != clientHandler
+                .getPlayer()) {
+            Logger.logwarn(clientHandler.getPlayer().getName()
+                    + " is trying to place reinforcement to "
+                    + country.getName() + " (" + country.getOwner().getName()
+                    + ")");
             sendError(ErrorCmd.ILLEGAL_ARGUMENT);
             return;
         }
-        
-        gameCtrl.setAvailableReinforcement(gameView.getAvailableReinforcement() - cmd.getTroops());
+
+        gameCtrl.setAvailableReinforcement(gameView.getAvailableReinforcement()
+                - cmd.getTroops());
         gameCtrl.addTroopsToCountry(country, cmd.getTroops());
-        cmdSender.sendCmd(new PlaceReinforcementCmd(country, cmd.getTroops(), gameView.getCurrentPlayer()), null);
-        
+        cmdSender.sendCmd(new PlaceReinforcementCmd(country, cmd.getTroops(),
+                gameView.getCurrentPlayer()), null);
+
         if (gameView.getAvailableReinforcement() == 0) {
             initNextPhase();
         }
@@ -294,24 +313,28 @@ public class ServerCommandVisitor implements CommandVisitor {
     public void visit(RegroupCmd cmd) {
         Country from = gameView.getCountry(cmd.getCountryPair().From.getName());
         Country to = gameView.getCountry(cmd.getCountryPair().To.getName());
-        Logger.logdebug("Got regroup command " + from.getName() + " -> " + to.getName() + " : " + cmd.getTroops());
-        if (from.getOwner() != to.getOwner() || from.getOwner() != clientHandler.getPlayer()) {
+        Logger.logdebug("Got regroup command " + from.getName() + " -> "
+                + to.getName() + " : " + cmd.getTroops());
+        if (from.getOwner() != to.getOwner()
+                || from.getOwner() != clientHandler.getPlayer()) {
             Logger.logerror("Invalid regroup command, owners do not match");
             sendError(ErrorCmd.ILLEGAL_ARGUMENT);
             return;
         }
-        if (!((gameView.getRoundPhase() == RoundPhase.ATTACK && gameView.getNextRoundPhase() == RoundPhase.REGROUP) ||
-                gameView.getRoundPhase() == RoundPhase.REGROUP))
-        {
+        if (!((gameView.getRoundPhase() == RoundPhase.ATTACK && gameView
+                .getNextRoundPhase() == RoundPhase.REGROUP) || gameView
+                .getRoundPhase() == RoundPhase.REGROUP)) {
             Logger.logerror("Regroup in wrong game phase!");
             sendError(ErrorCmd.INVALID_PHASE);
             return;
         }
         Attack lastAttack = gameView.getLastAttack();
-        if ((from != lastAttack.getCountryPair().From || to != lastAttack.getCountryPair().To) &&
-                gameView.getRoundPhase() != RoundPhase.REGROUP)
-        {
-            initNextPhase();
+        if (lastAttack != null) {
+            if ((from != lastAttack.getCountryPair().From || to != lastAttack
+                    .getCountryPair().To)
+                    && gameView.getRoundPhase() != RoundPhase.REGROUP) {
+                initNextPhase();
+            }
         }
         CountryPair cp = new CountryPair(from, to);
         gameCtrl.regroup(cp, cmd.getTroops());
@@ -322,21 +345,24 @@ public class ServerCommandVisitor implements CommandVisitor {
     public void visit(AttackStartCmd cmd) {
         Country from = gameView.getCountry(cmd.getCountryPair().From.getName());
         Country to = gameView.getCountry(cmd.getCountryPair().To.getName());
-        
-        Logger.logdebug("Got AttackStart command " + from.getName() + " -> " + to.getName());
-        
-        if (from.getOwner() != clientHandler.getPlayer() || to.getOwner() == clientHandler.getPlayer()) {
+
+        Logger.logdebug("Got AttackStart command " + from.getName() + " -> "
+                + to.getName());
+
+        if (from.getOwner() != clientHandler.getPlayer()
+                || to.getOwner() == clientHandler.getPlayer()) {
             Logger.logdebug("The owner of From or To countries are wrong");
             sendError(ErrorCmd.ILLEGAL_ARGUMENT);
-            return ;
+            return;
         }
-        
+
         if (gameView.getRoundPhase() != RoundPhase.ATTACK) {
-            Logger.logerror("Attack start in wrong phase! (" + gameView.getRoundPhase() + ")");
+            Logger.logerror("Attack start in wrong phase! ("
+                    + gameView.getRoundPhase() + ")");
             sendError(ErrorCmd.INVALID_PHASE);
             return;
         }
-        
+
         CountryPair cp = new CountryPair(from, to);
         gameCtrl.setAttack(new Attack(cp));
         cmdSender.sendCmd(new AttackStartCmd(cp), null);
@@ -348,7 +374,8 @@ public class ServerCommandVisitor implements CommandVisitor {
         Logger.logdebug("Got attackSetADiceCmd: " + dice);
         Country from = gameView.getAttack().getCountryPair().From;
         if (!(dice >= 1 && dice <= 3 && dice < from.getTroops())) {
-            Logger.logdebug("Too many (" + dice + ") adice! (troops: " + from.getTroops() + ")");
+            Logger.logdebug("Too many (" + dice + ") adice! (troops: "
+                    + from.getTroops() + ")");
             sendError(ErrorCmd.ILLEGAL_ARGUMENT);
             return;
         }
@@ -360,16 +387,17 @@ public class ServerCommandVisitor implements CommandVisitor {
     public void visit(AttackSetDDiceCmd cmd) {
         int dice = cmd.getDDice();
         Logger.logdebug("Got attackSetDDiceCmd: " + dice);
-        
+
         if (gameView.getAttack().getAttackerDice() < 1) {
             Logger.logerror("Defender cannot choose dice number before attacker does!");
             sendError(ErrorCmd.INVALID_PHASE);
             return;
         }
-        
+
         Country to = gameView.getAttack().getCountryPair().To;
-        if (!(dice >= 1 && dice <=2 && dice <= to.getTroops())) {
-            Logger.logdebug("Too many (" + dice + ") ddice! (troops: " + to.getTroops() + ")");
+        if (!(dice >= 1 && dice <= 2 && dice <= to.getTroops())) {
+            Logger.logdebug("Too many (" + dice + ") ddice! (troops: "
+                    + to.getTroops() + ")");
             sendError(ErrorCmd.ILLEGAL_ARGUMENT);
             return;
         }
@@ -382,24 +410,25 @@ public class ServerCommandVisitor implements CommandVisitor {
         Logger.logdebug("Doing attack");
         Attack attack = gameView.getAttack();
         Random r = new Random();
-        
+
         ArrayList<Integer> aDice = new ArrayList<Integer>();
         for (int i = 0; i < attack.getAttackerDice(); i++) {
-            Integer rand = r.nextInt(6) + 1; 
+            Integer rand = r.nextInt(6) + 1;
             aDice.add(rand);
             Logger.logdebug("Attacker thrown " + rand);
         }
-        
+
         ArrayList<Integer> dDice = new ArrayList<Integer>();
         for (int i = 0; i < attack.getDefenderDice(); i++) {
-            Integer rand = r.nextInt(6) + 1; 
+            Integer rand = r.nextInt(6) + 1;
             dDice.add(rand);
             Logger.logdebug("Defender thrown " + rand);
         }
-        
+
         gameCtrl.setAttackRoundResults(aDice, dDice);
         int[] losses = attack.calcLosses();
-        Logger.logdebug("Attacker lost: " + losses[0] + " defender lost: " + losses[1]);
+        Logger.logdebug("Attacker lost: " + losses[0] + " defender lost: "
+                + losses[1]);
         cmdSender.sendCmd(new AttackRoundResultCmd(aDice, dDice), null);
     }
 
